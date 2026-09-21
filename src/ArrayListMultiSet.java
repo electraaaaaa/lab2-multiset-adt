a02d54da79f93321d562c74377bd166676b52314
ngjus <justin329547@gmail.com>
2026-09-21 14:43:40 -0400

    completed remove and contains changes

--- src/ArrayListMultiSet.java
+++ src/ArrayListMultiSet.java
@@ -23,7 +23,7 @@ public class ArrayListMultiSet extends MultiSet {
      */
     @Override
     void remove(int item) {
-        this.lst.remove(item);
+        lst.remove(item);
     }
 
     /**
@@ -34,7 +34,7 @@ public class ArrayListMultiSet extends MultiSet {
      */
     @Override
     boolean contains(int item) {
-        return this.lst.contains(item);
+        return lst.contains(item);
     }
 
     /**
